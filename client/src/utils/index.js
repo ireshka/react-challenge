ab2771d04017088dd28040589dd85fee0c2d88c8
ireshka <ireshka@users.noreply.github.com>
2022-02-01 03:04:55 +0100

    feat: add capitalizeFirstLetter util fn

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,3 +1,4 @@
+export { capitalizeFirstLetter } from './capitalizeFirstLetter';
 export { getBrowserLocales } from './getBrowserLocale';
 export { getRandomColor } from './getRandomColor';
 export { formatCentsToDollars, formatDollarsToCents } from './money';
