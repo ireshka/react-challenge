f3ccddeb4a68299a42a4b53192c539f0b22ca29d
ireshka <ireshka@users.noreply.github.com>
2022-02-15 17:40:05 +0100

    feat(budget-page): add Button for defining budget

--- client/src/pages/Budget.page.jsx
+++ client/src/pages/Budget.page.jsx
@@ -1,11 +1,16 @@
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import { Grid } from '@mui/material';
-import { ActionHeader, Card, Page } from 'ui';
+import { ActionHeader, Button, Card, Page } from 'ui';
+
+const DefineBudgetButton = (
+  <Button startIcon={<AddRoundedIcon />} text={'Zdefiniuj budżet'} />
+);
 
 export const BudgetPage = () => (
   <Page title="Budżet">
     <Card
       title={
-        <ActionHeader variant="h1" title="Budżet" renderActions={() => null} />
+        <ActionHeader variant="h1" title="Budżet" renderActions={() => DefineBudgetButton} />
       }
     >
       <Grid container>
