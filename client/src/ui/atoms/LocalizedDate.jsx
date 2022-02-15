0abff6fa993a915be30436e518cc2789c975806d
ireshka <ireshka@users.noreply.github.com>
2022-02-15 17:35:16 +0100

    refactor: add proptypes for <Money /> & <LocalizedDate />

--- client/src/ui/atoms/LocalizedDate.jsx
+++ client/src/ui/atoms/LocalizedDate.jsx
@@ -1,8 +1,11 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
+import * as PropTypes from 'prop-types';
 import { getBrowserLocales } from 'utils';
 
 export const LocalizedDate = ({ date: rawDate }) => {
   const browserLocale = getBrowserLocales();
   return new Date(rawDate).toLocaleString(browserLocale[0]);
+};
+
+LocalizedDate.propTypes = {
+  date: PropTypes.number.isRequired,
 };
--- client/src/ui/atoms/Money.jsx
+++ client/src/ui/atoms/Money.jsx
@@ -1,3 +1,8 @@
+import * as PropTypes from 'prop-types';
 import { formatCentsToDollars } from 'utils';
 
 export const Money = ({ inCents }) => <>{formatCentsToDollars(inCents)} PLN</>;
+
+Money.propTypes = {
+  inCents: PropTypes.number.isRequired,
+};
