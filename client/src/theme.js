22ee7224628930328312adfe46d0bfd6ca9a952c
ireshka <ireshka@users.noreply.github.com>
2022-02-24 21:13:39 +0100

    refactor(task-2): fix table styles

--- client/src/theme.js
+++ client/src/theme.js
@@ -331,6 +331,15 @@ theme = createTheme(theme, {
         },
       },
     },
+    MuiTableRow: {
+      styleOverrides: {
+        root: {
+          '&.Mui-selected': {
+            backgroundColor: '#F9FAFD',
+          },
+        },
+      },
+    },
   },
 });
 
