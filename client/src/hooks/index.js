f927a801e900e1e589ec4f95a9c40c8c6e813797
ireshka <ireshka@users.noreply.github.com>
2022-03-01 22:57:58 +0100

    feat: add hooks for using modals

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,4 +1,6 @@
+export { useBudgetModal } from './useBudgetModal';
 export { useBudgets } from './useBudgets';
 export { useDeleteBudget } from './useDeleteBudget';
 export { useDeleteLedger } from './useDeleteLedger';
 export { useLedgers } from './useLedgers';
+export { useLedgersModal } from './useLedgersModal';
