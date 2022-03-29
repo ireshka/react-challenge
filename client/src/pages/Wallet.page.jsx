8245999121e58002afe9a4bad0535cd316f9d4cd
ireshka <ireshka@users.noreply.github.com>
2022-03-29 21:59:00 +0200

    fix: grid on Wallet page

--- client/src/pages/Wallet.page.jsx
+++ client/src/pages/Wallet.page.jsx
@@ -4,14 +4,20 @@ import { BudgetChart, LedgerWidget, Page, SummaryChart } from 'ui';
 export const WalletPage = () => (
   <Page title="Portfel">
     <Grid container spacing={{ xs: 3, md: 6 }}>
-      <Grid item xs={12} md={8}>
+      <Grid item xs={12} lg={8}>
         <LedgerWidget />
       </Grid>
-      <Grid container item xs={12} md={4} spacing={3}>
-        <Grid item xs={12} data-test-id={'wallet-top-sidebar'}>
+      <Grid container item xs={12} lg={4} spacing={3}>
+        <Grid item xs={12} md={6} lg={12} data-test-id={'wallet-top-sidebar'}>
           <SummaryChart />
         </Grid>
-        <Grid item xs={12} data-test-id={'wallet-bottom-sidebar'}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          lg={12}
+          data-test-id={'wallet-bottom-sidebar'}
+        >
           <BudgetChart />
         </Grid>
       </Grid>
